3856e28a4889559a7ce0b8636d8f429dfff2a85b
Franco Gilli <francogilli10@gmail.com>
2023-12-19 17:14:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import GoogleCalendarIframe from './components/GoogleCalendarIframe';
+
+const App = () => {
+  return (
+    <div className="">
+      <GoogleCalendarIframe  />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/GoogleCalendarIframe.jsx
@@ -0,0 +1,14 @@
+// GoogleCalendarIframe.js
+import React from 'react';
+
+const GoogleCalendarIframe = () => {
+  return (
+    <iframe
+      src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3_pwqiQZyP3Ye65Up99sO1I-3zLI8EBsgE25WgLCsAtrlz28Tut-vRF3VAx2gSAooLfEtALtTE?gv=true"
+      style={{ border: '0', width: '100%', height: '600px',marginTop: '0px' }}
+      
+    ></iframe>
+  );
+};
+
+export default GoogleCalendarIframe;
