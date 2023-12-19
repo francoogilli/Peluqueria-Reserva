e6d3ab3787e55b9cbb4a655f6e77911af3b9b021
Franco Gilli <francogilli10@gmail.com>
2023-12-19 17:27:37 -0300

    Fixed a component visual issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-// App.js
+// App.jsx
 import React from 'react';
 import GoogleCalendarIframe from './components/GoogleCalendarIframe';
+import './App.css'; // Importa un archivo CSS para personalizar el estilo si es necesario
 
 const App = () => {
   return (
-    <div className="">
-      <GoogleCalendarIframe  />
+    <div className="app-container">
+      <GoogleCalendarIframe />
     </div>
   );
 };
--- src/components/GoogleCalendarIframe.jsx
+++ src/components/GoogleCalendarIframe.jsx
@@ -1,12 +1,11 @@
-// GoogleCalendarIframe.js
+// GoogleCalendarIframe.jsx
 import React from 'react';
 
 const GoogleCalendarIframe = () => {
   return (
     <iframe
       src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3_pwqiQZyP3Ye65Up99sO1I-3zLI8EBsgE25WgLCsAtrlz28Tut-vRF3VAx2gSAooLfEtALtTE?gv=true"
-      style={{ border: '0', width: '100%', height: '600px',marginTop: '0px' }}
-      
+      style={{ border: '0', width: '100%', height: '100vh', marginTop: '0px' }}
     ></iframe>
   );
 };
